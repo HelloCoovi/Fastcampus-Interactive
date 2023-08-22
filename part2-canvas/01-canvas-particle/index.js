93b5df06f547c837022bfc453ebdbe2b56943dd4
jangg0627 <jangg0627@naver.com>
2023-08-23 01:02:36 +0900

    🎯👨‍🎓💄 `canvas`기본기 및 dpr 이해 - dpr 이해하기   -> `window.devicePixelRatio`로 사용자의 디스플레이의 dpi 값을 변수로 가져옴   -> 고해상도 디스플레이(맥의 레티나)의 경우 dpi값이 더 높을수 있기때문에 사용자의 dpi를 받아 값으로 사용   -> 🎯 canvas의 크기를 dpr 만큼 곱하고 동시에 scale을 사용해서 canvas크기도 동시에 늘림
    - canvas의 크기 정하기
      -> canvas는 canvas를 감싸는 html요소와 canvas 자체가 따로 존재하기 때문에 하나의 상수로 동시에 설정
      -> `canvasWidth`, `canvasHeight`를 기반으로 크기 설정
    
    - `fillRect`의 각 숫자들은 `x`, `y`, `width`, `height`를 의미
      -> 해당 죄표에 해당 너비와 높이를 가진 사각형을 그림

--- part2-canvas/01-canvas-particle/index.js
+++ part2-canvas/01-canvas-particle/index.js
@@ -1,5 +1,15 @@
 const canvas = document.querySelector("canvas")
-const ctx = canvas.getContext("2d")
 
-console.log(canvas)
-console.log(ctx)
+const ctx = canvas.getContext("2d")
+const dpr = window.devicePixelRatio
+
+const canvasWidth = 300
+const canvasHeight = 300
+
+canvas.style.width = canvasWidth + "px"
+canvas.style.height = canvasHeight + "px"
+canvas.width = canvasWidth * dpr
+canvas.height = canvasHeight * dpr
+ctx.scale(dpr, dpr)
+
+ctx.fillRect(10, 10, 50, 50)
