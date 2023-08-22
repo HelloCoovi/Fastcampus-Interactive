a2d5656b943ce780e17502a7610780709cf5ab50
jangg0627 <jangg0627@naver.com>
2023-08-23 01:35:13 +0900

    🎯👨‍🎓💄 canvas로 원 그리기 - 캔버스로 원을 그리는 순서  1. `beginPath`로 원을 그리는것을 시작하는것을 알리기  2. `arc`로 좌표, 반지름, 시작각도, 끝 각도 설정    -> 각도는 radian 방식으로 계산하며 1radian은 약 57.3으로 계산된다.  3. 스타일 설정  4. 채우기 or 선 그리기 (둘 다 가능)  5. 원 그리기 종료 - 🎯radian방식을 기억하자

--- part2-canvas/01-canvas-particle/index.js
+++ part2-canvas/01-canvas-particle/index.js
@@ -13,3 +13,13 @@ canvas.height = canvasHeight * dpr
 ctx.scale(dpr, dpr)
 
 ctx.fillRect(10, 10, 50, 50)
+
+ctx.beginPath()
+// X시작위치, Y시작위치, 반지름, 시작각도, 끝나는 각도(radian계산법)
+// 🎯 Math.PI / 180은 radian계산법으로 1 이며 이것에 360을 곱함으로써 360을 만드는것
+ctx.arc(100, 100, 50, 0, Math.PI / 180 * 360)
+// 원을 채울 색상을 고르고 fill()로 채워줌
+ctx.fillStyle = "red"
+ctx.fill()
+ctx.stroke()  // 채우기 없이 선 그리기
+ctx.closePath()
