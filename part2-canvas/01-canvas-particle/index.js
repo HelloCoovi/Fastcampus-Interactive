3e762adf8e90e4d5fefe2dcc95c564afb3b1b5c1
jangg0627 <jangg0627@naver.com>
2023-08-23 16:48:06 +0900

    🎯✨ 랜덤 위치에 파티클 그리기 - 파티클의 갯수, 랜덤한 숫자 생성함수 구현 - 위 변수로 임의의 `Particle` 객체를 생성 및 배열에 담고 forEach로 각 객체를 순회-그리기

--- part2-canvas/01-canvas-particle/index.js
+++ part2-canvas/01-canvas-particle/index.js
@@ -23,16 +23,28 @@ class Particle {
   draw() {
     ctx.beginPath()
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI / 180 * 360)
-    ctx.fillStyle = "red"
+    ctx.fillStyle = "orange"
     ctx.fill()
     ctx.closePath()
   }
 }
 
-const x = 100
-const y = 100
-const radius = 50
-const particle = new Particle(x, y, radius)
+const TOTAL = 5
+const randomNumBetween = (min, max) => {
+  return Math.random() * (max - min + 1) + min
+}
+
+const anime = () => {
+
+}
+let particles = []
+
+for (let i = 0; i < TOTAL; i++) {
+  const x = randomNumBetween(0, canvasWidth)
+  const y = randomNumBetween(0, canvasHeight)
+  const radius = randomNumBetween(50, 100)
+  particles.push(new Particle(x, y, radius))
+}
 
 let interval = 1000 / 60
 let now, delta
@@ -48,8 +60,10 @@ function animate() {
 
   // 캔버스 전체를 지우고 y 그리기
   ctx.clearRect(0, 0, canvasWidth, canvasHeight)
-  particle.y += 1 // 시작지점(좌측 상단)에서 y방향으로+(밑으로 내림)
-  particle.draw()
+
+  particles.forEach((particle => {
+    particle.draw()
+  }))
 
   then = now - (delta % interval)
 }
