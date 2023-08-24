37200d821768a280f2364fdb0c35d1bd9176e03e
jangg0627 <jangg0627@naver.com>
2023-08-24 17:50:45 +0900

    🎯🧰🧑‍💻 `dat.gui` 세팅 - 조작하려는 값을 `querySelector`로 가져와 수정 - `dat.gui`의 사용법이 주를 이루며 자세한 내용은 주석 참고

--- part2-canvas/01-canvas-particle/index.js
+++ part2-canvas/01-canvas-particle/index.js
@@ -14,6 +14,51 @@ ctx.scale(dpr, dpr)
 
 ctx.fillRect(10, 10, 50, 50)
 
+// dat.gui 생성자
+let gui = new dat.GUI()
+
+// 도대체 강의에서 이 코드를 왜 쓰는지 이해가 안됌
+// const controls = new function () {
+//   this.blurValue = 40
+//   this.alphaChannel = 100
+//   this.alphaOffset = -23
+//   this.acc = 1.03
+// }
+
+// ⬇️ 임의로 수정한 코드
+// 조작할 요소 지정
+const controls = {
+  blurValue: 40,
+  alphaChannel: 100,
+  alphaOffset: -23,
+  acc: 1.03
+}
+
+const feGaussianBlur = document.querySelector('feGaussianBlur')
+const feColorMatrix = document.querySelector('feColorMatrix')
+
+// 폴더 만들기
+const f1 = gui.addFolder('Gooey Effect')
+// 폴더가 열려있게 설정
+f1.open()
+// 특정 폴더에 컨트롤할 값 넣기 폴더를 원치 않는다면 gui.add(~~)로 기입
+f1.add(controls, 'blurValue', 0, 100).onChange(value => {
+  feGaussianBlur.setAttribute('stdDeviation', value)
+})
+// 🎯조작하는 하나의 값을 조작하면서 다른 값을 유지하기위해 조작하지 않는 값은 controls를 확인하게 설정
+f1.add(controls, 'alphaChannel', 1, 200).onChange(value => {
+  feColorMatrix.setAttribute('values', `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${value} ${controls.alphaOffset}`)
+})
+f1.add(controls, 'alphaOffset', -40, 40).onChange(value => {
+  feColorMatrix.setAttribute('values', `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${controls.alphaChannel} ${value}`)
+})
+
+const f2 = gui.addFolder('Particle Property')
+f2.open()
+f2.add(controls, 'acc', 1, 1.5, 0.01).onChange(value => {
+  particles.forEach(particle => particle.acc = value)
+})
+
 class Particle {
   constructor(x, y, radius, vy) {
     this.x = x
