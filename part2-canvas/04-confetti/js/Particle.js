5b2aea25e42b2ad01288e35680652f4275b354ac
jangg0627 <jangg0627@naver.com>
2023-08-29 22:28:46 +0900

    🎯💫 특정 방향으로 파티클 발사하기 - `deg` 변수를 받아 -30 ~ 30 범위(부채꼴)에 파티클이 생기게 설정 - 발사력(`r`)과 중력-마찰을 조정해서 더 자연스럽게 구현

--- part2-canvas/04-confetti/js/Particle.js
+++ part2-canvas/04-confetti/js/Particle.js
@@ -1,20 +1,20 @@
 import { randomNumBetween } from "./utils.js"
 
 export default class Particle {
-  constructor(x, y) {
+  constructor(x, y, deg = -50) {
     this.x = x
     this.y = y
 
     this.width = 30
     this.height = 30
 
-    this.r = 7
-    this.angle = Math.PI / 180 * randomNumBetween(0, 360)
+    this.r = randomNumBetween(30, 100)
+    this.angle = Math.PI / 180 * randomNumBetween(deg - 30, deg + 30)
 
     this.vx = this.r * Math.cos(this.angle)
     this.vy = this.r * Math.sin(this.angle)
-    this.friction = 0.97
-    this.gravity = 0.1
+    this.friction = 0.89
+    this.gravity = 0.5
   }
   update() {
     this.vy += this.gravity
