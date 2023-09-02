011f6e394d335209827a1bb14d0d08c3e42e3459
jangg0627 <jangg0627@naver.com>
2023-09-02 15:54:55 +0900

    🎯✨ **장애물 랜덤 배치** 구현 - 랜덤값 생성 함수 구현 - 장애물과 장애물 사이 `gapY` 구현 - 위에 있는 장애물 위치 `y1` 구현   -> 전체 높이 만큼 끌어 올리고 여윳값(30)부터 전체 높이 - gap 만큼 내려오게 설정 - 아래있는 장애물 `y2` 구현   -> 기존 `y1`과 `gap`을 기반으로 간단히 구현
    - `y1`, `y2` 그려주기
    - 오버라이드(덮어쓰기)로 원하는 값을 찾음

--- part2-canvas/05-mini-game/js/Wall.js
+++ part2-canvas/05-mini-game/js/Wall.js
@@ -1,4 +1,5 @@
 import App from "./App.js"
+import { randomNumBetween } from "./utils.js"
 
 export default class Wall {
   constructor(config) {
@@ -18,6 +19,20 @@ export default class Wall {
 
     this.width = App.height * this.sizeX
     this.height = App.height
+
+    // 전체 높이값의 20% ~ 35% (아래 this.gapY는 오버라이드 코드)
+    this.gapY = randomNumBetween(App.height * 0.2, App.height * 0.35)
+    // this.gapY = App.height * 0.5
+
+    this.x = 0
+
+    // 전체높이까지 + (30 ~ (전체높이 - 사이값 - 30))
+    // 최소한 gapY만큼의 공간을 확보하고(App.height - this.gapY) 나머지는 랜덤으로 지정
+    this.y1 = -this.height + randomNumBetween(30, App.height - this.gapY - 30)
+    // this.y1 = this.height * 0.5
+
+    // 구해진 y1 + 전체높이(요소 높이) + gap
+    this.y2 = this.y1 + this.height + this.gapY
   }
 
   update() {
@@ -28,7 +43,12 @@ export default class Wall {
       // 이미지 자르기 옵션
       // sx: 자르기 시작할 X위치, sy: 자르기 시작할 Y위치, 너비, 높이
       this.sx, 0, this.sizeX * this.img.width, this.img.height,
-      0, 0, this.width, this.height
+      this.x, this.y1, this.width, this.height
+    )
+    App.ctx.drawImage(
+      this.img,
+      this.sx, 0, this.sizeX * this.img.width, this.img.height,
+      this.x, this.y2, this.width, this.height
     )
   }
 }
