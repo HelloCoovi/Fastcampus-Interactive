986d5d6110075ae93b649e1d5d120272f8dbc680
jangg0627 <jangg0627@naver.com>
2023-09-03 13:45:02 +0900

    🎯💫 스프라이트 이미지 비율을 사용해 **하나의 스프라이트 이미지 그리기** - 원본 이미지의 크기를 확인해서 비율 계산식으로 확대값 적용 - `drawImage`의 자르기 옵션으로 단일 스프라이트 출력   -> `frameX`는 이미지의 위치를 의미 - `width`와 `height`는 비율이 계산된 값으로 지정

--- part2-canvas/05-mini-game/js/Player.js
+++ part2-canvas/05-mini-game/js/Player.js
@@ -5,14 +5,20 @@ export default class Player {
     this.img = document.querySelector('#bird-img')
     this.x = App.width * 0.1
     this.y = App.height * 0.5
+    this.width = 130
+    // 원본이미지의 스프라이트 1개의 크기의 가로세로 비율로 이미지 크기 설정
+    // 원본이미지 크기: 3990 x 207 (3990 / 15 == 266)
+    this.height = this.width * (207 / 266)
 
+    this.frameX = 0
   }
   update() {
   }
   draw() {
     App.ctx.drawImage(
       this.img,
-      this.x, this.y, 100, 100
+      this.img.width / 15 * this.frameX, 0, this.img.width / 15, this.img.height,
+      this.x, this.y, this.width, this.height
     )
   }
 }
