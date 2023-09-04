f18c192aaacedfcb45aa271f6a845edbb4a9b5bc
jangg0627 <jangg0627@naver.com>
2023-09-04 18:55:27 +0900

    🎯💫 **coin 위치에 바운딩 박스 생성** - `Coin.js`에서 `boundingBox`를 받음 - 기존 x, y 값에 미리 로직을 적용해서 `boundingBox`와 `coin` 객체에 적용

--- part2-canvas/05-mini-game/js/Coin.js
+++ part2-canvas/05-mini-game/js/Coin.js
@@ -1,18 +1,21 @@
 import App from "./App.js"
+import BoundingBox from "./BoundingBox.js"
 
 export default class Coin {
   constructor(x, y, vx) {
     this.img = document.querySelector("#coin-img")
 
-    this.x = x
-    this.y = y
     this.width = 70
     this.height = 70
+    this.x = x - this.width * 0.5
+    this.y = y - this.height * 0.5
 
     this.counter = 0
     this.frameX = 0
 
     this.vx = vx
+
+    this.boundingBox = new BoundingBox(this.x, this.y, this.width, this.height)
   }
 
   get isOutside() {
@@ -27,13 +30,16 @@ export default class Coin {
     }
 
     this.x += this.vx
+    this.boundingBox.x = this.x
 
   }
   draw() {
     App.ctx.drawImage(
       this.img,
       this.img.width / 6 * this.frameX, 0, this.img.width / 6, this.img.height,
-      this.x - this.width * 0.5, this.y - this.height * 0.5, this.width, this.height
+      this.x, this.y, this.width, this.height
     )
+
+    this.boundingBox.draw()
   }
 }
