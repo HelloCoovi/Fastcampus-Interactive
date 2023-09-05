68fe95c192cf6995d4de48243d19983f1389e7ea
jangg0627 <jangg0627@naver.com>
2023-09-05 22:27:58 +0900

    ✨💫 **게임 화면이 시작할때 애니메이션** 구현 - `gsap`으로 `gameTitle`에 간단한 애니메이션 삽입   -> CSS에 초기값 지정

--- part2-canvas/05-mini-game/js/GameHandler.js
+++ part2-canvas/05-mini-game/js/GameHandler.js
@@ -1,5 +1,20 @@
 export default class GameHandler {
   constructor() {
-    this._status = "READY"
+    this._status = "READY" // READY, PLAYING, FINISHED
+
+    this.init()
+    this.showReadyScreen()
+  }
+
+  init() {
+    this.readyScreen = document.querySelector(".ready-screen")
+    this.gameTitle = this.readyScreen.querySelector(".game-title")
+    this.playBtn = this.readyScreen.querySelector(".play-btn")
+  }
+
+  showReadyScreen() {
+    gsap.to(this.gameTitle, {
+      scale: 1, rotation: 720, opacity: 1, duration: 0.5
+    })
   }
 }
