04acd7234097d02b31181dfd062019059947cdf1
jangg0627 <jangg0627@naver.com>
2023-09-05 22:56:15 +0900

    🎯💫 **play-btn에 gsap 애니메이션 설정** - playBtn에 gsap 애니메이션 적용 - 아래 사이트에서 애니메이션을 보고 골라보자 https://greensock.com/docs/v2/Easing - 🎯이 문서는 2버전이며 3가 최신 문법임

--- part2-canvas/05-mini-game/js/GameHandler.js
+++ part2-canvas/05-mini-game/js/GameHandler.js
@@ -9,12 +9,16 @@ export default class GameHandler {
   init() {
     this.readyScreen = document.querySelector(".ready-screen")
     this.gameTitle = this.readyScreen.querySelector(".game-title")
-    this.playBtn = this.readyScreen.querySelector(".play-btn")
+    this.playBtn = this.readyScreen.querySelectorAll(".play-btn")
   }
 
   showReadyScreen() {
     gsap.to(this.gameTitle, {
       scale: 1, rotation: 720, opacity: 1, duration: 0.5
+    })
+    gsap.to(this.playBtn, {
+      scale: 1, duration: 1, ease: Elastic.easeOut.config(2, 0.5), delay: 0.5
+
     })
   }
 }
