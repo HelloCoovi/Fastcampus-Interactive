36935f3911bc4c676731adc0b47fd11bb90109a0
jangg0627 <jangg0627@naver.com>
2023-09-06 01:31:22 +0900

    🎯✨ **버튼이 눌리면 `status`를 playing으로 업데이트**하게 구현 - 버튼이 눌리면 `hideReadyScreen()` 동작   -> gsap으로 애니메이션 실행 + status 할당(setter 실행)

--- part2-canvas/05-mini-game/js/GameHandler.js
+++ part2-canvas/05-mini-game/js/GameHandler.js
@@ -19,7 +19,12 @@ export default class GameHandler {
   init() {
     this.readyScreen = document.querySelector(".ready-screen")
     this.gameTitle = this.readyScreen.querySelector(".game-title")
-    this.playBtn = this.readyScreen.querySelectorAll(".play-btn")
+    this.playBtns = this.readyScreen.querySelectorAll(".play-btn")
+    this.playBtns.forEach(btn => {
+      btn.addEventListener("click", () => {
+        this.hideReadyScreen();
+      });
+    });
 
     this.status = "READY"
   }
@@ -28,8 +33,16 @@ export default class GameHandler {
     gsap.to(this.gameTitle, {
       scale: 1, rotation: 720, opacity: 1, duration: 0.5
     })
-    gsap.to(this.playBtn, {
+    gsap.to(this.playBtns, {
       scale: 1, duration: 1, ease: Elastic.easeOut.config(2, 0.5), delay: 0.5
+    })
+  }
+
+  hideReadyScreen() {
+    gsap.to(this.readyScreen, {
+      opacity: 0, pointerEvents: "none", duration: 0.3, onComplete: () => {
+        this.status = "PLAYING"
+      }
     })
   }
 }
