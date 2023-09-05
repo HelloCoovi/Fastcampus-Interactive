1fea1c9ca9f6503c4648397b335e0538c1b3a8f3
jangg0627 <jangg0627@naver.com>
2023-09-05 16:38:54 +0900

    🎯✨💫 coin과 거리(distCount)를 보여주는 글자 출력 - 거리와 coin을 관리하는 변수 생성 - 🎯`canvas`의 `Text` 옵션을 사용해서 글씨 출력 - 🧩 구글 폰트에서 폰트 파일을 html로 가져옴

--- part2-canvas/05-mini-game/js/Score.js
+++ part2-canvas/05-mini-game/js/Score.js
@@ -5,11 +5,23 @@ export default class Score {
   constructor() {
     this.coin = new Coin(App.width - 50, 50, 0)
     this.coin.frameX = 0
+
+    this.distCount = 0
+    this.coinCount = 0
+
   }
   update() {
 
   }
   draw() {
     this.coin.draw()
+
+    App.ctx.font = "55px Jua"
+    App.ctx.fillStyle = "#f1f1f1"
+    App.ctx.textAlign = "right"
+    App.ctx.fillText(this.coinCount, App.width - 90, 69)
+
+    App.ctx.textAlign = "left"
+    App.ctx.fillText(`${this.distCount}m`, 25, 69)
   }
 }
