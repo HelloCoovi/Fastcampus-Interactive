85173d00f539795914fe460eef6d0936f6ae6dde
jangg0627 <jangg0627@naver.com>
2023-09-06 15:36:03 +0900

    🎯✨💫 finished 로직 설정 - `readyScreen`과 유사하게 로직 구현 - gsap 애니메이션 지정   -> `finishScreen`이 나타나는 애니메이션 설정     => display값을 flex로 보이게 설정     => coin과 dist 텍스트가 자연스럽게 나오게 설정 - 🥅 단순히 `pointer-events`로는 해결이 안돼서 `display: none`을 활용함

--- part2-canvas/05-mini-game/js/GameHandler.js
+++ part2-canvas/05-mini-game/js/GameHandler.js
@@ -13,36 +13,67 @@ export default class GameHandler {
 
     switch (value) {
       case "READY": this.showReadyScreen(); break
+      case "FINISHED": this.showFinishScreen(); break
     }
   }
 
   init() {
     this.readyScreen = document.querySelector(".ready-screen")
     this.gameTitle = this.readyScreen.querySelector(".game-title")
-    this.playBtns = this.readyScreen.querySelectorAll(".play-btn")
-    this.playBtns.forEach(btn => {
-      btn.addEventListener("click", () => {
-        this.hideReadyScreen();
-      });
-    });
+    this.playBtn = this.readyScreen.querySelector(".play-btn")
+    this.playBtn.addEventListener("click", () => {
+      this.hideReadyScreen();
+    })
+
+    this.finishScreen = document.querySelector(".finish-screen")
+    this.distanceText = this.finishScreen.querySelector(".distance")
+    this.coinText = this.finishScreen.querySelector(".coin")
+    this.replayBtn = this.finishScreen.querySelector(".replay-btn")
+    this.replayBtn.addEventListener("click", () => {
+      this.hideFinishScreen();
+    })
 
     this.status = "READY"
+    // this.status = "FINISHED"
   }
 
   showReadyScreen() {
     gsap.to(this.gameTitle, {
       scale: 1, rotation: 720, opacity: 1, duration: 0.5
     })
-    gsap.to(this.playBtns, {
-      scale: 1, duration: 1, ease: Elastic.easeOut.config(2, 0.5), delay: 0.5
+    gsap.to(this.playBtn, {
+      scale: 1, duration: 1, ease: Elastic.easeOut.config(2, 0.5), delay: 0.5, pointerEvents: "all"
     })
   }
-
   hideReadyScreen() {
     gsap.to(this.readyScreen, {
-      opacity: 0, pointerEvents: "none", duration: 0.3, onComplete: () => {
+      opacity: 0, pointerEvents: "none", duration: 0.3, display: "none", onComplete: () => {
         this.status = "PLAYING"
       }
     })
+  }
+
+  showFinishScreen() {
+    gsap.fromTo(this.finishScreen, { opacity: 0, display: "none" }, {
+      opacity: 1, duration: 0.5, display: "flex"
+    })
+    gsap.fromTo(this.distanceText, { opacity: 0, scale: 0 }, {
+      opacity: 1, scale: 1, duration: 0.5, delay: 1
+    })
+    gsap.fromTo(this.coinText, { opacity: 0, scale: 0 }, {
+      opacity: 1, scale: 1, duration: 0.5, delay: 1.1
+    })
+    gsap.to(this.distanceText, {
+      duration: 0.5, y: -80, delay: 1.5
+    })
+    gsap.to(this.coinText, {
+      duration: 0.5, y: -80, delay: 1.5
+    })
+    gsap.fromTo(this.replayBtn, { opacity: 0, scale: 0 }, {
+      opacity: 1, scale: 1, rotation: 720, duration: 0.5, y: 50, delay: 1.5
+    })
+  }
+  hideFinishScreen() {
+
   }
 }
