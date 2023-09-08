41ff1d0a0d45cb01693bfaf9e691851bbcd37649
jangg0627 <jangg0627@naver.com>
2023-09-08 23:23:27 +0900

    🎯🧠✨ 삼각함수를 사용해서 두 점의 길이가 계속해서 유지되는 로직 구현 - 삼각 머시기 피타고라스 머시깽이 하기 때문에 나중에 헷갈린다면 part2-ch06-05 강의를 다시 보자
    - 두 지점이 늘어난 길이만큼 계속해서 가까워지게 공식을 설정해서 물리 효과처럼 보이게 구현
    
    - 겁나 어렵네;

--- part2-canvas/06-rope-effect/js/Stick.js
+++ part2-canvas/06-rope-effect/js/Stick.js
@@ -2,9 +2,27 @@ export default class Stick {
   constructor(p1, p2) {
     this.startPoint = p1
     this.endPoint = p2
+
+    this.length = this.startPoint.pos.dist(this.endPoint.pos)
   }
   update() {
+    const dx = this.endPoint.pos.x - this.startPoint.pos.x
+    const dy = this.endPoint.pos.y - this.startPoint.pos.y
 
+    const dist = Math.sqrt(dx * dx + dy * dy)
+    const diff = (dist - this.length) / dist
+
+    const offsetX = diff * dx
+    const offsetY = diff * dy
+
+    if (!this.startPoint.pinned) {
+      this.startPoint.pos.x += offsetX * 0.5
+      this.startPoint.pos.y += offsetY * 0.5
+    }
+    if (!this.endPoint.pinned) {
+      this.endPoint.pos.x -= offsetX * 0.5
+      this.endPoint.pos.y -= offsetY * 0.5
+    }
   }
   draw(ctx) {
     ctx.beginPath()
