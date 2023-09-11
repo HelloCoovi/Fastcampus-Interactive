35fbfb689558e659d8683d68463a78d1f5893bb1
jangg0627 <jangg0627@naver.com>
2023-09-11 22:58:12 +0900

    💄🧩 프로젝트 전체 스타일 설정 + 구글 폰트 - 프로젝트에서 사용할 컬러 값 세팅 - 구글 폰트 불러오기

--- part2-canvas/07-react+canvas/canvas-portfolio/src/App.jsx
+++ part2-canvas/07-react+canvas/canvas-portfolio/src/App.jsx
@@ -1,11 +1,5 @@
-import { useEffect } from "react";
-
 function App() {
-  useEffect(() => {
-    console.log("프로젝트 실행!");
-  }, []);
-
-  return <div>프로젝트 초기화!</div>;
+  return <div className="app">project init</div>;
 }
 
 export default App;
