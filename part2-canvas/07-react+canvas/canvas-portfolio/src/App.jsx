98ae9dde4ef4119497b4413965dfd4870c17f3d1
jangg0627 <jangg0627@naver.com>
2023-09-11 23:22:48 +0900

    💄🔨 UI 세팅 - 전체적인 html과 CSS를 세팅함 - 🎯요소에 `transform: scaleX(-1);`를 넣으면 요소가 반전됨

--- part2-canvas/07-react+canvas/canvas-portfolio/src/App.jsx
+++ part2-canvas/07-react+canvas/canvas-portfolio/src/App.jsx
@@ -1,5 +1,55 @@
+import ArrowImg from "./assets/arrow.svg";
+
 function App() {
-  return <div className="app">project init</div>;
+  return (
+    <>
+      <div className="app">
+        <section className="section-1">
+          <header>
+            <h1>Portfolio</h1>
+            <ul>
+              <li>instagram</li>
+              <li>twitter</li>
+              <li>codePen</li>
+            </ul>
+          </header>
+          <main>
+            <div>canvas</div>
+          </main>
+        </section>
+        <section className="section-2">What is Lorem Ipsum?</section>
+        <section className="section-3">
+          <aside>
+            <div className="top">1914 translation by H.</div>
+            <div className="bottom">
+              <img src={ArrowImg} />
+              <img src={ArrowImg} />
+            </div>
+          </aside>
+          <article>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an
+            unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+            typesetting, remaining essentially unchanged. It was in the 1960s with the release of sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+            software like PageMaker including versions of Lorem Ipsum.
+          </article>
+        </section>
+        <section className="section-4">
+          <canvas></canvas>
+          <aside>
+            <h1>Javascript</h1>
+            <h2>⭐️ ⭐️ ⭐️ ⭐️ ⭐️</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an
+              unknown
+            </p>
+          </aside>
+        </section>
+      </div>
+      <footer>
+        <div className="email">test@gmail.com</div>
+      </footer>
+    </>
+  );
 }
 
 export default App;
