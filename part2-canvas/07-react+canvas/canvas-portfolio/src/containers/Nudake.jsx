ae7a9f5219823286e2868f8bb54062ae825d5c41
jangg0627 <jangg0627@naver.com>
2023-09-12 19:01:21 +0900

    🎯👨‍🎓💫 React에서 canvas로 애니메이션 그리기 - 간단한 사각형이 튕기는 애니메이션 그리기

--- part2-canvas/07-react+canvas/canvas-portfolio/src/containers/Nudake.jsx
+++ part2-canvas/07-react+canvas/canvas-portfolio/src/containers/Nudake.jsx
@@ -16,17 +16,41 @@ function Nudake() {
       canvasWidth = canvasParent.clientWidth;
       canvasHeight = canvasParent.clientHeight;
       canvas.style.width = `${canvasWidth}px`;
-      canvas.style.height = `${canvasHeight}` + "px";
+      canvas.style.height = `${canvasHeight}px`;
       canvas.width = canvasWidth;
       canvas.height = canvasHeight;
     }
+
+    let x = 0;
+    let y = 0;
+    let vx = 20;
+    let vy = 20;
+    let size = 200;
 
     let frameId;
 
     function frame() {
       frameId = requestAnimationFrame(frame);
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-      ctx.fillRect(100, 100, 100, 100);
+      if (x < 0) {
+        x = 0;
+        vx *= -1;
+      } else if (x > canvasWidth - size) {
+        x = canvasWidth - size;
+        vx *= -1;
+      } else if (y < 0) {
+        y = 0;
+        vy *= -1;
+      } else if (y > canvasHeight - size) {
+        y = canvasHeight - size;
+        vy *= -1;
+      }
+
+      x += vx;
+      y += vy;
+
+      ctx.fillRect(x, y, size, size);
     }
 
     window.addEventListener("resize", resize);
