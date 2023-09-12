76a86c4ab134ff2f821e88dbecdc06437d6f53c0
jangg0627 <jangg0627@naver.com>
2023-09-12 22:20:01 +0900

    🎯✨ canvas로 이미지 그리기 - 이미지를 불러오고 배열의 index로 접근해서 이미지 그리기

--- part2-canvas/07-react+canvas/canvas-portfolio/src/containers/Nudake.jsx
+++ part2-canvas/07-react+canvas/canvas-portfolio/src/containers/Nudake.jsx
@@ -2,6 +2,10 @@ import { useEffect, useRef } from "react";
 
 import "../style/containers/Nudake.css";
 
+import image1 from "../assets/nudake-1.jpg";
+import image2 from "../assets/nudake-2.jpg";
+import image3 from "../assets/nudake-3.jpg";
+
 function Nudake() {
   const canvasRef = useRef(null);
 
@@ -9,6 +13,9 @@ function Nudake() {
     const canvas = canvasRef.current;
     const canvasParent = canvas.parentNode;
     const ctx = canvas.getContext("2d");
+
+    const imageSrcs = [image1, image2, image3];
+    let currIndex = 0;
 
     let canvasWidth, canvasHeight;
 
@@ -19,6 +26,17 @@ function Nudake() {
       canvas.style.height = `${canvasHeight}px`;
       canvas.width = canvasWidth;
       canvas.height = canvasHeight;
+
+      drawImage();
+    }
+
+    function drawImage() {
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+      const image = new Image();
+      image.src = imageSrcs[currIndex];
+      image.onload = () => {
+        ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight);
+      };
     }
 
     window.addEventListener("resize", resize);
