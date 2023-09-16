dfa8fa817e1ce32862acfa685dba0262649f9d30
jangg0627 <jangg0627@naver.com>
2023-09-16 17:52:36 +0900

    📦✨ `Matter.js` 설치 및 기본 설정 - `npm install matter-js` - 기본적인 Matter.js 세팅   -> 애니메이션을 위한 기본 요소     => engine, render, runner, composite   -> 마우스 관련 요소     => mouse, mouseConstraint

--- part2-canvas/07-react+canvas/canvas-portfolio/src/containers/RotateCanvas.jsx
+++ part2-canvas/07-react+canvas/canvas-portfolio/src/containers/RotateCanvas.jsx
@@ -1,9 +1,50 @@
 import "../style/containers/RotateCanvas.css";
 
+import {
+  Engine,
+  Render,
+  Runner,
+  Mouse,
+  MouseConstraint,
+  Composite,
+} from "matter-js";
+import { useEffect, useRef } from "react";
+
 function RotateCanvas() {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const cw = 1000;
+    const ch = 1000;
+
+    const engine = Engine.create();
+    const render = Render.create({
+      canvas: canvas,
+      engine: engine,
+      options: {
+        width: cw,
+        height: ch,
+        wireframes: false,
+        background: "#1b1b19",
+      },
+    });
+    const runner = Runner.create();
+
+    Render.run(render);
+    Runner.run(runner, engine);
+
+    const mouse = Mouse.create(canvas);
+    const mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+    });
+
+    Composite.add(engine.world, mouseConstraint);
+  }, []);
+
   return (
     <div className="rotate-canvas-wrapper">
-      <canvas></canvas>
+      <canvas ref={canvasRef}></canvas>
       <aside>
         <h1>Javascript</h1>
         <h2>⭐️ ⭐️ ⭐️ ⭐️ ⭐️</h2>
