b10bcfb0e306a49d7e4a42cbaea6686641551d6e
jangg0627 <jangg0627@naver.com>
2023-09-20 02:35:02 +0900

    🎯✨ **기본적인 cube 세팅 + 카메라 세팅** - 요소 생성후 scene에 추가 - camera의 위치와 보는 방향 설정(`lookAt`) - 요소와 카메라가 처음 생겼을때는 초기위치로 생성됨   -> 위치 등을 조절하지 않으면 제대로 보이지 않아서 조정 필요
    - antialias으로 그래픽의 계단 현상을 해결

--- part3-three-js-basic/1-cube/src/main.js
+++ part3-three-js-basic/1-cube/src/main.js
@@ -5,7 +5,10 @@ window.addEventListener("load", function () {
 })
 
 function init() {
-  const renderer = new THREE.WebGLRenderer()
+  const renderer = new THREE.WebGLRenderer({
+    // alpha: true,  // 검정 배경 지우기(투명한 배경)
+    antialias: true  // 렌더링시 계단 현상 해결(안티 앨리어싱)
+  })
 
   renderer.setSize(window.innerWidth, window.innerHeight)
 
@@ -19,6 +22,18 @@ function init() {
     1, // 최소 시야 거리
     500  // 최대 시야 거리
   )
+
+  const geometry = new THREE.BoxGeometry(2, 2, 2) // 높이, 너비, 깊이
+  const material = new THREE.MeshBasicMaterial({ color: 0xcc99ff })
+
+  const cube = new THREE.Mesh(geometry, material)
+
+  scene.add(cube)
+
+  // camera.position.z = 5
+  camera.position.set(3, 4, 5) // 카메라 좌표 설정 x, y, z
+
+  camera.lookAt(cube.position)
 
   renderer.render(scene, camera)
 }
