21b08a44de0479fb3c34ff110d88af4ab581480f
jangg0627 <jangg0627@naver.com>
2023-09-20 02:17:22 +0900

    🎯✨ **기본적인 Three.js 설정** - 새로운 Three.js 객체 `renderer` 생성   -> `setSize`로 랜더링 할 화면 크기 설정   -> renderer를 dom에 추가 - 보여줄 장면 `scene` 객체 생성 - 시야인 `camera` 객체 생성
    - `renderer`에서 `render`

--- part3-three-js-basic/1-cube/src/main.js
+++ part3-three-js-basic/1-cube/src/main.js
@@ -5,5 +5,20 @@ window.addEventListener("load", function () {
 })
 
 function init() {
-  console.log(THREE)
+  const renderer = new THREE.WebGLRenderer()
+
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  document.body.appendChild(renderer.domElement)
+
+  const scene = new THREE.Scene()
+
+  const camera = new THREE.PerspectiveCamera(
+    75, // 시야각
+    window.innerWidth / window.innerHeight, // 종횡비
+    1, // 최소 시야 거리
+    500  // 최대 시야 거리
+  )
+
+  renderer.render(scene, camera)
 }
