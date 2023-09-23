80f82b9401fd1850e033a8211e4de015339b97ba
jangg0627 <jangg0627@naver.com>
2023-09-23 22:53:55 +0900

    🎯✨ `material`를 십이면체로 변경 - 기존 주석을 없애고 필요한 코드를 남김 - 십이면체 + 잘 보이는 재질로 변경 - `emissive`: 빛이 닿지 않는 부분이 보이는 색상   -> 기본값은 검정 - cube의 x, y에 회전(`rotation`) 효과 지정   -> `clock`의 `getElapsedTime`로 애니메이션 간격 설정

--- part3-three-js-basic/1-cube/src/main.js
+++ part3-three-js-basic/1-cube/src/main.js
@@ -23,47 +23,34 @@ function init() {
     500  // 최대 시야 거리
   )
 
-  const geometry = new THREE.BoxGeometry(2, 2, 2) // 높이, 너비, 깊이
-  const material = new THREE.MeshStandardMaterial({
-    // color: 0xcc99ff
-    color: new THREE.Color(0xcc99ff),
-    // transparent: true,
-    // opacity: 0.5,
-    // visible: false,
-    // wireframe: true,
-    // side: THREE.DoubleSide,
+  const geometry = new THREE.IcosahedronGeometry(1)
+  const material = new THREE.MeshLambertMaterial({
+    color: 0x00ffff,
+    emissive: 0x111111,
   })
-  material.color = new THREE.Color(0x00cb96)
+  // material.color = new THREE.Color(0x00cb96)
 
   const cube = new THREE.Mesh(geometry, material)
 
   scene.add(cube)
 
-  // camera.position.z = 5
-  camera.position.set(3, 4, 5) // 카메라 좌표 설정 x, y, z
+  camera.position.z = 5
+  // camera.position.set(3, 4, 5) // 카메라 좌표 설정 x, y, z
 
   camera.lookAt(cube.position)
 
-  const directionalLight = new THREE.DirectionalLight(0xf0f0f0, 1)
-  directionalLight.position.set(-1, 2, 3)
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
   scene.add(directionalLight)
-
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.1)
-  ambientLight.position.set(3, 2, 1)
-  scene.add(ambientLight)
 
   const clock = new THREE.Clock()
 
   render()
 
   function render() {
-    // cube.rotation.x += THREE.MathUtils.degToRad(1)
-    // cube.rotation.y = Math.sin(cube.rotation.x)
-    // cube.scale.x = Math.cos(cube.rotation.x)
+    const elapsedTime = clock.getElapsedTime()
 
-    // cube.rotation.x = Date.now() / 1000
-    // cube.rotation.x = clock.getElapsedTime() // clock 객체가 생성된 후 경과한 시간
-    cube.rotation.x += clock.getDelta() // clock 객체 호출 사이 간격
+    cube.rotation.x = elapsedTime
+    cube.rotation.y = elapsedTime
 
     renderer.render(scene, camera)
 
